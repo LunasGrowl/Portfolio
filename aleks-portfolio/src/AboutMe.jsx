e637e4bb1cfac22ab613027f1fc9c82c1dacc631
LunasGrowl <abychawski@gmail.com>
2023-11-11 15:47:27 +0000

    Starting the about me sections

--- aleks-portfolio/src/AboutMe.jsx
+++ aleks-portfolio/src/AboutMe.jsx
@@ -1,5 +1,21 @@
 export default function AboutMe(){
     return(
-        <p>Big black men</p>
+        <div className="aboutMe--container">
+            <h2>About</h2>
+            <h1>Learn About Me</h1>
+            <div className="aboutMe--content">
+                <img></img>
+                <h2>Graduate Computer Science Student From the University of Portsmouth</h2>
+                <p>Just soem mumbo jumbo about me</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <p>Some more mumbo jumbo to talk about</p>
+            </div>
+        </div>
     )
 }
