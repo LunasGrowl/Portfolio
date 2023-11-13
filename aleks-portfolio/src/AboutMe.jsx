3ed6efd9869831afec44bb1934d8ef0bc7f419e7
LunasGrowl <abychawski@gmail.com>
2023-11-13 16:09:37 +0000

    about me work

--- aleks-portfolio/src/AboutMe.jsx
+++ aleks-portfolio/src/AboutMe.jsx
@@ -4,18 +4,20 @@ export default function AboutMe(){
             <h2 className = "section--title">About</h2>
             <h1 className="section--subTitle">Learn About Me</h1>
             <div className="aboutMe--content">
-                <img className="aboutMe--image" src="./src/images/Aleks-croped.jpg"/>
-                <h2>Graduate Computer Science Student From the University of Portsmouth</h2>
-                <p>Just soem mumbo jumbo about me</p>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <p>Some more mumbo jumbo to talk about</p>
+                <img className="aboutMe--image" src="./src/images/Aleks-croped.jpg" />
+                <div className="aboutMe--information">
+                    <h2 className="aboutMe--position">Graduate Computer Science Student From the University of Portsmouth</h2>
+                    <p className="aboutMe--">Just soem mumbo jumb about me</p>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
             </div>
+            <p>Some more mumbo jumbo to talk about</p>
         </div>
     )
 }
--- aleks-portfolio/src/main.jsx
+++ aleks-portfolio/src/main.jsx
@@ -6,6 +6,7 @@ import './index.css'
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Edu+TAS+Beginner:wght@500&display=swap');
 </style>
 
 ReactDOM.createRoot(document.getElementById('root')).render(
