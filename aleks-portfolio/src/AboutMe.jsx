6eb67623157714afa88d63b982159a61d9885228
LunasGrowl <abychawski@gmail.com>
2023-11-13 15:19:56 +0000

    Image uploaded

--- aleks-portfolio/src/AboutMe.jsx
+++ aleks-portfolio/src/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe(){
             <h2 className = "section--title">About</h2>
             <h1 className="section--subTitle">Learn About Me</h1>
             <div className="aboutMe--content">
-                <img src="./src/images/Aleks.HEIC"></img>
+                <img className="aboutMe--image" src="./src/images/Aleks-croped.jpg"/>
                 <h2>Graduate Computer Science Student From the University of Portsmouth</h2>
                 <p>Just soem mumbo jumbo about me</p>
                 <ul>
